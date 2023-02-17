c143afde43d230894dc3241bb649dbd3bd5938ba
Mishachkld <mishachkld@gmail.com>
2023-02-17 23:14:57 +0200

    Initial commit

---
+++ CustomString/CustomString.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Mishach on 14.02.2023.
+//
+
+#include "CustomString.hpp"
+
+namespace ms {
+    CustomString::CustomString() {
+
+    }
+
+    CustomString::~CustomString() {
+
+    }
+} // ms
---
+++ CustomString/CustomString.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Mishach on 14.02.2023.
+//
+#pragma once
+#define SIZE_STRING
+namespace ms {
+
+    class CustomString {
+    public:
+        CustomString();
+        ~CustomString();
+    private:
+        char string[SIZE_STRING];
+    };
+
+} // ms
